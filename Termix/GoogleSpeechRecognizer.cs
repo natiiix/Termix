d55d365fa933904c20e002406cb235f6a7e8bb8b
Ivo Meixner <ivo.meixner@se7en.cz>
2018-01-14 23:40:54 +0100

    Command actions are now performed as async tasks

--- Termix/GoogleSpeechRecognizer.cs
+++ Termix/GoogleSpeechRecognizer.cs
@@ -71,13 +71,17 @@ namespace Termix
                             if (result.IsFinal)
                             {
                                 dtTimeout = DateTime.Now + RECOGNITION_TIMEOUT_AFTER_FINAL;
-                                FinalRecognitionAction(alternative.Transcript);
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+                                Task.Factory.StartNew(() => FinalRecognitionAction(alternative.Transcript));
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
                             }
                             // Recognition continues
                             else
                             {
                                 dtTimeout = DateTime.Now + RECOGNITION_TIMEOUT_PARTIAL;
-                                PartialRecognitionAction(alternative.Transcript);
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+                                Task.Factory.StartNew(() => PartialRecognitionAction(alternative.Transcript));
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
                             }
                         }
                     }
