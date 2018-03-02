24ef2eeb9907d2f3085de130dd8af1adce284699
Ivo Meixner <ivo.meixner@se7en.cz>
2018-03-03 00:25:13 +0100

    Code cleanup

--- Termix/MainWindow.xaml.cs
+++ Termix/MainWindow.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.Windows;
 using System.Globalization;
+using System.Windows;
 
 namespace Termix
 {
--- Termix/WindowsTools.cs
+++ Termix/WindowsTools.cs
@@ -2,7 +2,6 @@
 using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Windows.Input;
-using System.Linq;
 
 namespace Termix
 {
