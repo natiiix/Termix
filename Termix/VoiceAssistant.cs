317f27d0c1e8325bb9d4ab0527a966b194a491ac
Ivo Meixner <ivo.meixner@se7en.cz>
2018-03-04 02:29:23 +0100

    Further improved the YouTube mix command

--- Termix/VoiceAssistant.cs
+++ Termix/VoiceAssistant.cs
@@ -119,7 +119,7 @@ namespace Termix
             RegisterCommand("open weather forecast", ActionOpenWeatherForecast);
             RegisterCommand(@"how much is (\d+(?:.\d+)?) (\+|-|\*|/) (\d+(?:.\d+)?)", ActionSolveMathProblem);
             RegisterCommand("how much is (.+)", ActionGoogleMathProblem);
-            RegisterCommand("play (?:me )?(?:something from (.+?)|(?:a )?(.+?) (?:YouTube )?mix)(?: on YouTube)?", ActionPlayYouTubeMix);
+            RegisterCommand("play (?:me )?(?:some(?:thing from)? (.+?)|(?:a )?(.+?) (?:YouTube )?mix)(?: on YouTube)?", ActionPlayYouTubeMix);
             RegisterCommand("(?:what is|what's) the time|what time is it", ActionReadTime);
             RegisterCommand("(?:tell|read) (?:me )?a joke", ActionReadJoke);
 
